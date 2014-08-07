9b3467fb9feebcda860a6950b54489bad922646d
Nishant Kumar <nishant.kr10@gmail.com>
2014-08-08 00:42:51 +0530

    Initial Commit

---
+++ hyperSpaceDrive/hyperSpaceDrive/GameOver.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Navigation;
+using Microsoft.Phone.Controls;
+using Microsoft.Phone.Shell;
+
+namespace hyperSpaceDrive
+{
+    public partial class GameOver : PhoneApplicationPage
+    {
+        public GameOver()
+        {
+            InitializeComponent();
+            netsc.Text = Level.score.ToString();
+            if (Level.score == Level.hl1)
+            {
+                TextBlock tx = new TextBlock();
+                tx.Text = "Congrats !! HIGH SCORE !!";
+            }
+        }
+
+        private void returnHome(object sender, RoutedEventArgs e)
+        {
+            NavigationService.Navigate(new Uri("/MainPage.xaml", UriKind.Relative));
+        }
+
+        protected override void OnBackKeyPress(System.ComponentModel.CancelEventArgs e)
+        {
+            e.Cancel = true;
+            base.OnBackKeyPress(e);
+        }
+
+    }
+}
---
+++ hyperSpaceDrive/hyperSpaceDrive/Level.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO.IsolatedStorage;
+using System.IO;
+
+
+namespace hyperSpaceDrive
+{
+
+    public static class Level
+    {
+        public static int curlevel=1;
+        public static int hl1=282;
+        public static int hl2=0;
+        public static int score=0;
+        public static bool isNewScoreHigh=false ;
+       
+        
+        public static void loadfn()
+        {
+            IsolatedStorageFile store = IsolatedStorageFile.GetUserStoreForApplication();
+            if (store.FileExists("test.txt")) // Check if file exists
+            {
+                IsolatedStorageFileStream save = new IsolatedStorageFileStream("test.txt", FileMode.Open, store);
+                BinaryReader reader = new BinaryReader(save);
+                string mystring = reader.ReadString();
+                hl1 = (int)reader.ReadSingle();
+                reader.Close();
+            }
+        }
+
+        public static void strfn()
+        {
+            IsolatedStorageFile store = IsolatedStorageFile.GetUserStoreForApplication(); // grab the storage
+            FileStream stream = store.OpenFile("test.txt", FileMode.Create); // Open a file in Create mode
+            BinaryWriter writer = new BinaryWriter(stream);
+            writer.Write("SCORE : ");
+            writer.Write(hl1);
+            writer.Close();
+        }
+    }
+}
